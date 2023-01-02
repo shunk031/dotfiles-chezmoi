f41d4d109c4f0ae3eddc20f3e69efab33160922d
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-03 02:39:36 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -Eeuox pipefail
+
+declare -r DOTFILES_LOGO="
+                          /$$                                      /$$
+                         | $$                                     | $$
+     /$$$$$$$  /$$$$$$  /$$$$$$   /$$   /$$  /$$$$$$      /$$$$$$$| $$$$$$$
+    /$$_____/ /$$__  $$|_  $$_/  | $$  | $$ /$$__  $$    /$$_____/| $$__  $$
+   |  $$$$$$ | $$$$$$$$  | $$    | $$  | $$| $$  \ $$   |  $$$$$$ | $$  \ $$
+    \____  $$| $$_____/  | $$ /$$| $$  | $$| $$  | $$    \____  $$| $$  | $$
+    /$$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$/| $$$$$$$//$$ /$$$$$$$/| $$  | $$
+   |_______/  \_______/   \___/   \______/ | $$____/|__/|_______/ |__/  |__/
+                                           | $$
+                                           | $$
+                                           |__/
+                     *** This is dotfiles setup script ***
+"
+
+function initialize_dotfiles() {
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/shunk031/dotfiles-chezmoi
+}
+
+function main() {
+    echo "$DOTFILES_LOGO"
+
+    initialize_dotfiles
+}
+
+main "$@"
